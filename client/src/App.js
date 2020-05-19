925c6df9fcbfec3247d1ec4afb30e3d5149adef7
sameerdewan <sameer.dewan@itential.com>
2020-05-19 12:05:36 -0400

    updates

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,62 @@ import React from 'react';
 import {withRouter, Switch, Route, Redirect} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import Spinner from 'react-bootstrap/Spinner';
 import NavBar from './components/Navbar'
 import StarDetails from './components/StarDetails';
 import SearchStars from './components/SearchStars';
+import Web3 from 'web3';
+import starNotaryArtifact from '../../build/contracts/StarNetwork.json';
 
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      web3Enabled: false
+    };
+    this.web3 = undefined;
+    this.meta = undefined;
+    this.account = undefined;
+  }
+
+  componentDidMount() {
+    if (window.ethereum) {
+      this.web3 = new Web3(window.ethereum);
+      this.enableWeb3();
+    }
+  }
+
+  async enableWeb3() {
+    await window.ethereum.enable();
+    const networkId = await this.web3.eth.net.getId();
+    const deployedNetwork = starNotaryArtifact.networks[networkId];
+    this.meta = new this.web3.eth.Contract(starNotaryArtifact.abi, deployedNetwork.address);
+    const accounts  = await this.web3.eth.getAccounts();
+    this.account = accounts[0];
+    this.setState({web3Enabled: true});
+  }
+
+  async lookupStar() {
+
+  }
 
   render() {
+    if (this.state.web3Enabled === false) return (
+      <div className={'center'}>
+        <Spinner animation="grow" variant="primary" size="lg" />
+        <br/><br/>
+        Searching for web3...
+      </div>
+    );
     return (
       <React.Fragment>
-        <NavBar history={this.props.history}/>
+        <NavBar history={this.props.history} account={this.account}/>
         <Switch>
           <Route exact path={'/'}>
             <SearchStars history={this.props.history}/>
           </Route>
-          <Route exact path={'/stars/:starName'} component={StarDetails} />
+          <Route exact path={'/stars/:starName'} render={(props) => <StarDetails {...props} account={this.account} web3={this.web3} />} />
           <Redirect to={{pathname: "/"}} />
         </Switch>
       </React.Fragment>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from 'react-bootstrap/Nav'
 
 class NavBar extends React.Component {
     render() {
@@ -7,6 +9,14 @@ class NavBar extends React.Component {
         <React.Fragment>
           <Navbar bg="primary">
             <Navbar.Brand onClick={() => this.props.history.push('/')}>StarNetwork</Navbar.Brand>
+            <Navbar.Collapse>
+              <Nav>
+                <span className={'account-address'}>{this.props.account}</span>
+                <NavDropdown title="" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">My Stars</NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
           </Navbar>
         </React.Fragment>
       )
